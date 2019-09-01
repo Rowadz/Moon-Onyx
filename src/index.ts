da343cbd73649029312b1223b28f81d005ec78de
MohammedAl-rowad <rowadwo@gmail.com>
2019-09-01 16:26:29 +0300

    added bootstrap and particales js

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,13 @@
 import '../src/styles/index.scss';
 import * as jquery from 'jquery';
+import 'bootstrap';
+import 'particles.js';
+import neuralNetwork from './json/particles/neural-network.json';
+
+declare const particlesJS: any;
 
 (window as any).$ = (window as any).jQuery = jquery;
+
+particlesJS('particles-js', neuralNetwork);
+
+requestAnimationFrame(() => {});
