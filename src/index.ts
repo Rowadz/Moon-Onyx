66281673d7b5f7f483f8d98a4b3900088e7e8816
MohammedAl-rowad <rowadwo@gmail.com>
2019-09-01 15:26:06 +0300

    added scss loader

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import '../src/styles/index.scss';
 import * as jquery from 'jquery';
 
 (window as any).$ = (window as any).jQuery = jquery;
--- webpack.config.js
+++ webpack.config.js
@@ -53,7 +53,18 @@ var config = {
           }
         : null,
       { test: /\.html$/, loader: 'html-loader' },
-      { test: /\.css$/, loaders: ['style-loader', 'css-loader'] }
+      { test: /\.css$/, loaders: ['style-loader', 'css-loader'] },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader'
+        ]
+      }
     ].filter(Boolean)
   },
   resolve: {
