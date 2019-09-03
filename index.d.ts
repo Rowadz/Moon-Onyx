0f06dec06f11ed5743eb2badc5938bc9bf7d634d
MohammedAl-rowad <rowadwo@gmail.com>
2019-09-04 02:35:43 +0300

    :sad:

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,5 @@
 import '../src/styles/index.scss';
 import 'jqueryui';
-import '../src/lib/jquery.ui.touch-punch.min.js';
 import 'bootstrap';
 import 'particles.js';
 import './typescript/optionsMenu/options.behavior';
