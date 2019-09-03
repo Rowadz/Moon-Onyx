f16377791efb895491495e5977d6bae2abbde506
MohammedAl-rowad <rowadwo@gmail.com>
2019-09-04 02:36:06 +0300

    :sad:

--- dist/json/particles/colors.helpers.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const mainColor = "#F2F2F2";
-export declare const mainLinkedLargeWidth = "#0D1A26";
--- dist/typescript/optionsMenu/input.logic.d.ts
+++
@@ -1,1 +0,0 @@
-export declare const createCardWithInpt: (defaultVal?: string) => void;
