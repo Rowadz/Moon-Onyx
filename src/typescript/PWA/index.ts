3c0dc78a350074b8a77adca75428e0a44dcc5738
MohammedAl-rowad <rowadwo@gmail.com>
2019-09-04 23:07:09 +0300

    swjs

--- src/typescript/PWA/index.ts
+++ src/typescript/PWA/index.ts
@@ -1,26 +1,5 @@
-// if ('serviceWorker' in navigator) {
-//   navigator.serviceWorker.register('/sw.js').then(function() {
-//     console.log('Service Worker Registered');
-//   });
-// }
-const cacheName = 'hello-world-page';
-const filesToCache = ['/', '/index.html'];
-self.addEventListener('install', (e: any) => {
-  console.log('[ServiceWorker] Install');
-  e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      console.log('[ServiceWorker] Caching app shell');
-      return cache.addAll(filesToCache);
-    })
-  );
-});
-self.addEventListener('activate', (event: any) => {
-  event.waitUntil((self as any).clients.claim());
-});
-self.addEventListener('fetch', (event: any) => {
-  event.respondWith(
-    caches.match(event.request, { ignoreSearch: true }).then(response => {
-      return response || fetch(event.request);
-    })
-  );
-});
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('./sw.js').then(function() {
+    console.log('Service Worker Registered');
+  });
+}
---
+++ src/typescript/PWA/sw.js
@@ -0,0 +1,21 @@
+const cacheName = 'hello-world-page';
+const filesToCache = ['/', '/index.html'];
+self.addEventListener('install', e => {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request, { ignoreSearch: true }).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
