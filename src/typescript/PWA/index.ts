98e53f47d40ff8ad0a54cde76ba960e782e1f302
MohammedAl-rowad <rowadwo@gmail.com>
2019-09-04 23:09:00 +0300

    png

--- src/typescript/PWA/index.ts
+++ src/typescript/PWA/index.ts
@@ -1,5 +1,9 @@
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('./sw.js').then(function() {
-    console.log('Service Worker Registered');
-  });
+  navigator.serviceWorker
+    .register(
+      'https://raw.githubusercontent.com/MohammedAl-Rowad/Moon-Onyx/code/src/typescript/PWA/sw.js'
+    )
+    .then(function() {
+      console.log('Service Worker Registered');
+    });
 }
